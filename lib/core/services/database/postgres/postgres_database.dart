0cdd47ddcd0e2b4ab70f1a3651eb953f6ee8fefc
Phil Silveira <phills.silveira@gmail.com>
2022-08-25 22:19:50 -0300

    ✨ add remove-database service and postgres impl

---
+++ lib/core/services/database/postgres/postgres_database.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:postgres/postgres.dart';
+import 'package:social_media_rest_api/core/services/database/remote_database.dart';
+import 'package:social_media_rest_api/core/services/dot_env/dot_env_service.dart';
+
+class PostgresDatabase implements RemoteDatabase {
+  final completer = Completer<PostgreSQLConnection>();
+  final DotEnvService _dotEnv;
+
+  PostgresDatabase(this._dotEnv) {
+    _init();
+  }
+
+  _init() async {
+    final uri = Uri.parse(_dotEnv['DATABASE_URL'] ?? '');
+
+    final connection = PostgreSQLConnection(
+      uri.host,
+      uri.port,
+      uri.pathSegments.first,
+      username: uri.userInfo.split(':').first,
+      password: uri.userInfo.split(':').last,
+    );
+    await connection.open();
+    completer.complete(connection);
+  }
+
+  @override
+  Future<List<Map<String, Map<String, dynamic>>>> query(
+    String queryText, {
+    Map<String, String> variables = const {},
+  }) async {
+    final connection = await completer.future;
+
+    return await connection.mappedResultsQuery(
+      queryText,
+      substitutionValues: variables,
+    );
+  }
+}
