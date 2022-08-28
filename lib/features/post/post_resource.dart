bce89df5e7774c216fb419311bf49b8aa9b44eb0
Phil Silveira <phills.silveira@gmail.com>
2022-08-28 12:14:21 -0300

    ✨ add post CRUD actions

--- lib/features/post/post_resource.dart
+++ lib/features/post/post_resource.dart
@@ -12,7 +12,7 @@ class PostResource extends Resource {
         Route.get(path, _getAllPosts),
         Route.get("$path/:id", _getPostByID),
         Route.post(path, _createPost),
-        Route.put(path, _updatePost),
+        Route.put("$path/:id", _updatePost),
         Route.delete("$path/:id", _deletePostByID),
       ];
 }
@@ -20,25 +20,68 @@ class PostResource extends Resource {
 FutureOr<Response> _getAllPosts(Injector injector) async {
   final result = await injector
       .get<RemoteDatabase>()
-      .query('SELECT * FROM "Post" ORDER BY "createdAt" DESC;');
+      .query('SELECT * FROM "post" ORDER BY "createdAt" DESC;');
 
-  final posts = result.map((i) => i['Post']).toList();
+  final posts = result.map((i) {
+    final item = i['post'];
+    item?.remove('createdAt');
+    return item;
+  }).toList();
 
   return Response(200, body: jsonEncode(posts));
 }
 
-FutureOr<Response> _getPostByID(ModularArguments args) {
-  return Response(200, body: "Get post ID (${args.params['id']})");
+FutureOr<Response> _getPostByID(
+  ModularArguments args,
+  Injector injector,
+) async {
+  final result = await injector.get<RemoteDatabase>().query(
+    'SELECT * FROM "post" WHERE id=@id;',
+    variables: {'id': args.params['id']},
+  );
+
+  if (result.isEmpty) return Response(404);
+
+  final post = result.first['post'];
+  post?.remove('createdAt');
+
+  return Response(200, body: jsonEncode(post));
 }
 
-FutureOr<Response> _createPost() {
-  return Response(200, body: "New post added");
+FutureOr<Response> _createPost(
+  ModularArguments args,
+  Injector injector,
+) async {
+  await injector.get<RemoteDatabase>().query(
+    'INSERT INTO public.post(text) VALUES (@text);',
+    variables: {'text': args.data['text']},
+  );
+  return Response(201, body: "New post added");
 }
 
-FutureOr<Response> _updatePost() {
+FutureOr<Response> _updatePost(
+  ModularArguments args,
+  Injector injector,
+) async {
+  await injector.get<RemoteDatabase>().query(
+    'UPDATE "post" SET text=@text WHERE id=@id;',
+    variables: {
+      'id': args.params['id'],
+      'text': args.data['text'],
+    },
+  );
+
   return Response(200, body: "Post updated");
 }
 
-FutureOr<Response> _deletePostByID(ModularArguments args) {
+FutureOr<Response> _deletePostByID(
+  ModularArguments args,
+  Injector injector,
+) async {
+  await injector.get<RemoteDatabase>().query(
+    ' DELETE FROM "post" WHERE id=@id;',
+    variables: {'id': args.params['id']},
+  );
+
   return Response(200, body: "Post ID (${args.params['id']}) deleted");
 }
